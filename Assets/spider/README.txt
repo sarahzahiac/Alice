fcccd6a918342abdcf833362a3c4adf0977c3efd
ikasaidi <ikramsaidi47@gmail.com>
2025-12-03 22:04:38 -0500

    deco piece du basement

---
+++ Assets/spider/README.txt
@@ -0,0 +1,20 @@
+SPIDER ANIMATIONS
+
+0-45 = Walk
+46-65 = Attack 1
+66-85 = Attack2
+86-99 = Eat
+100-120 = Defend
+121-134 = Hit 1
+135-149 = Hit 2
+150-155 = Crouch
+157-162 = Stand Tall
+164-213 = Idle 1
+214-249 = Idle 2
+250-269 = Jump
+270-279 = Side Step/Turn on the spot
+280-299 = Die 1
+300-329 = Die 2
+
+Psionic
+www.psionicgames.com
---
+++ Assets/spider/spider4unity/spider/Assets/spidercontrol.js
@@ -0,0 +1,47 @@
+﻿#pragma strict
+
+
+
+
+
+private var SPIDER : GameObject; SPIDER = GameObject.Find("spider1MESH");
+
+var texturenum = 1;
+var texture1 : Texture2D;
+var texture2 : Texture2D;
+var texture3 : Texture2D;
+var texture4 : Texture2D;
+var texture5 : Texture2D;
+
+function Update () {
+
+
+	if(Input.GetKeyDown(KeyCode.Space)){
+		//print("space key pressed");
+
+			if (texturenum == 1){
+				SPIDER.GetComponent(SkinnedMeshRenderer).material.SetTexture("_MainTex", texture2);
+				texturenum = 2;
+			}
+			else if (texturenum == 2){
+				SPIDER.GetComponent(SkinnedMeshRenderer).material.SetTexture("_MainTex", texture3);
+				texturenum = 3;
+			}
+			else if (texturenum == 3){
+				SPIDER.GetComponent(SkinnedMeshRenderer).material.SetTexture("_MainTex", texture4);
+				texturenum = 4;
+			}
+			else if (texturenum == 4){
+				SPIDER.GetComponent(SkinnedMeshRenderer).material.SetTexture("_MainTex", texture5);
+				texturenum = 5;
+			}
+			else if (texturenum == 5){
+				SPIDER.GetComponent(SkinnedMeshRenderer).material.SetTexture("_MainTex", texture1);
+				texturenum = 1;
+			}
+
+	} 
+}
+
+
+
